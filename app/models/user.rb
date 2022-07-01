9e4448db99826cd0d19e8794a757abd67b305ed2
Vladimir Kozyrev <vkozyrev@anna.money>
2022-07-01 09:25:04 +0400

    add annoate gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  image_url  :string
+#  name       :string
+#  nickname   :string
+#  token      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   validates :email, presence: true
   validates :image_url, presence: true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  image_url  :string
+#  name       :string
+#  nickname   :string
+#  token      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
