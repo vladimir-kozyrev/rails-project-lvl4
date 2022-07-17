f7b2418d37573ae6b5bde8ad320092329c5909dd
Vladimir Kozyrev <vkozyrev@anna.money>
2022-07-17 19:40:17 +0400

    add test for Ruby checks

--- test/jobs/repository_check_job_test.rb
+++ test/jobs/repository_check_job_test.rb
@@ -3,11 +3,19 @@
 require 'test_helper'
 
 class RepositoryCheckJobTest < ActiveJob::TestCase
-  test 'check fails if linter returns non-zero exit code' do
+  test 'JavsScript check fails if linter returns non-zero exit code' do
     check = repository_checks(:one)
     RepositoryCheckJob.perform_now(check)
     check.reload
     assert { check.failed? }
     assert { check.output == load_fixture('eslint_check_result.json') }
+  end
+
+  test 'Ruby check fails if linter returns non-zero exit code' do
+    check = repository_checks(:two)
+    RepositoryCheckJob.perform_now(check)
+    check.reload
+    assert { check.failed? }
+    assert { check.output == load_fixture('rubocop_check_result.json') }
   end
 end
