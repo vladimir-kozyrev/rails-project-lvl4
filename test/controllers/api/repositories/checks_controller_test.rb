fd627392faf19048130a511951bbd724f11aa212
Vladimir Kozyrev <vkozyrev@anna.money>
2022-07-18 19:12:22 +0400

    add tests for /api/checks

--- test/controllers/api/repositories/checks_controller_test.rb
+++ test/controllers/api/repositories/checks_controller_test.rb
@@ -3,8 +3,15 @@
 require 'test_helper'
 
 class Api::Repositories::ChecksControllerTest < ActionDispatch::IntegrationTest
-  # test 'should get create' do
-  #   get api_repositories_checks_create_url
-  #   assert_response :success
-  # end
+  test 'fail to create check with unknown repository' do
+    json_params = load_fixture('unknown_repository_webhook_payload.json')
+    post api_checks_url, params: JSON.parse(json_params)
+    assert_response :unprocessable_entity
+  end
+
+  test 'should create check for existing repository' do
+    json_params = load_fixture('existing_repository_webhook_payload.json')
+    post api_checks_url, params: JSON.parse(json_params)
+    assert_response :created
+  end
 end
