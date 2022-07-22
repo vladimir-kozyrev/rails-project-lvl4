48b7d46f2a44b01396a75ad36c7b9841bec166b8
Vladimir Kozyrev <vkozyrev@anna.money>
2022-07-22 11:29:51 +0400

    add tests for jobs

--- test/jobs/update_repository_metadata_job_test.rb
+++ test/jobs/update_repository_metadata_job_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class UpdateRepositoryMetadataJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'repository metadata is updated' do
+    repository = repositories(:one)
+    UpdateRepositoryMetadataJob.perform_now(repository.github_id, repository.user.token)
+    repository.reload
+    repository_dummy = JSON.parse(load_fixture('octokit_repo_response.json'))
+    assert { repository.name == repository_dummy['name'] }
+    assert { repository.owner_name == repository_dummy['owner']['login'] }
+  end
 end
