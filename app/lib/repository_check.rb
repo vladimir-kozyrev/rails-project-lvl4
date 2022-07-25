7cf95037ef86af48473b2bd3b0fe9d741d15561a
Vladimir Kozyrev <vkozyrev@anna.money>
2022-07-25 15:41:54 +0400

    refactoring, prevent check conflicts by cloning each repository to a unique path

--- app/lib/repository_check.rb
+++ app/lib/repository_check.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 require 'fileutils'
+require 'securerandom'
 
 class RepositoryCheck
   def self.download(repository)
-    repo_clone_path = "#{Rails.root}/tmp/repositories/#{repository.owner_name}_#{repository.name}"
-    FileUtils.rm_rf(repo_clone_path)
+    repo_clone_path = "#{Rails.root}/tmp/repositories/#{SecureRandom.uuid}"
     command = "git clone #{repository.link} #{repo_clone_path}"
     stderr, exit_status = Open3.popen3(command) do |_stdin, _stdout, stderr, wait_thr|
       [stderr.read, wait_thr.value]
@@ -13,35 +13,42 @@ class RepositoryCheck
     if exit_status.exitstatus != 0 && stderr.present?
       Rails.logger.error "Failed to clone repository #{repository.link} to #{repo_clone_path}"
       Rails.logger.error "stderr: #{stderr}"
-      return [nil, nil]
+      FileUtils.rm_rf(repo_clone_path) and return [nil, nil]
     end
     commit_hash = commit_hash(repo_clone_path)
     [repo_clone_path, commit_hash]
   end
 
   def self.check(repository_path, linter)
-    linter_command = determine_command(linter, repository_path)
-    stdout, stderr, exit_status = Open3.popen3(linter_command) do |_stdin, stdout, stderr, wait_thr|
-      [stdout.read, stderr.read, wait_thr.value]
-    end
+    linter_command = linter_command(linter, repository_path)
+    stdout, stderr, exit_status = lint(linter_command, repository_path)
     stdout.gsub!(/^[^\[]+/, '') if linter == 'eslint' && stdout.include?('yarn run')
-    if exit_status.exitstatus != 0 && stderr.present?
+    if exit_status != 0 && stderr.present?
       Rails.logger.warn "#{linter_command} did not complete successfully"
       Rails.logger.warn "stderr: #{stderr}"
     end
     # since stdout will be put into check.output,
     # we must set it to nil because JSON.parse("") throws an error
     stdout = nil if stdout.empty?
-    [stdout, exit_status.exitstatus]
+    [stdout, exit_status]
+  ensure
+    FileUtils.rm_rf(repository_path)
   end
 
-  def self.determine_command(linter, repository_path)
+  def self.linter_command(linter, repository_path)
     case linter
     when 'eslint'
       "yarn run eslint --no-eslintrc --format json #{repository_path}"
     when 'rubocop'
-      "rubocop --format json #{repository_path}/*"
+      "bundle exec rubocop --format json #{repository_path}/*"
     end
+  end
+
+  def self.lint(command, repository_path)
+    stdout, stderr, exit_status = Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
+      [stdout.read, stderr.read, wait_thr.value]
+    end
+    [stdout, stderr, exit_status.exitstatus]
   end
 
   def self.commit_hash(repo_clone_path)
