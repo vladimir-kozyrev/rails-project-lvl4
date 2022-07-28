cc5edc616b657caaf323c58aa5f0e1fcec881b46
Vladimir Kozyrev <vkozyrev@anna.money>
2022-07-28 16:48:36 +0400

    remove useless test

--- test/jobs/create_repository_hook_job_test.rb
+++ test/jobs/create_repository_hook_job_test.rb
@@ -2,13 +2,4 @@
 
 require 'test_helper'
 
-class CreateRepositoryHookJobTest < ActiveJob::TestCase
-  test 'repository hook is created' do
-    repository = repositories(:one)
-    repo_full_name = repository.full_name
-    github_token = repository.user.token
-    CreateRepositoryHookJob.perform_now(repo_full_name, github_token)
-    expected_response = JSON.parse(load_fixture('octokit_create_hook_response.json'))
-    assert { Octokiter.create_hook(repo_full_name, github_token) == expected_response }
-  end
-end
+class CreateRepositoryHookJobTest < ActiveJob::TestCase; end
